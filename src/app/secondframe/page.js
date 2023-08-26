8d25069fc7d598cff7435ce5f5d76fad0cd9754b
unknown <deepak09.singh@gmail.com>
2023-08-26 23:20:41 +0530

    Nextjs code updated

--- src/app/secondframe/page.js
+++ src/app/secondframe/page.js
@@ -7,9 +7,9 @@ import delhi from "./delhi.jpg";
 import Xcover from "./Xcover.jpg";
 import star1 from "./star1.jpg";
 import icon from "./icon.jpg";
-import plane from "./plane.jpg";
+import plane from "./Plane.jpg";
 import Vector from "./Vector.jpg";
-import Weight from "./Weight.png";
+import Weight from "./weight.png";
 import { Public_Sans } from "next/font/google";
 
 export default function page() {
